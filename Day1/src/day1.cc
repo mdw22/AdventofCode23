b393c06fec5c0936f82341fa8cbc15a0cbc9706b
mdw22 <mdw22@email.sc.edu>
2024-01-04 13:22:53 -0800

    Final cleanup of code

--- Day1/src/day1.cc
+++ Day1/src/day1.cc
@@ -130,42 +130,7 @@ std::tuple<int, int> wordToInt(std::string input, bool reverse) {
 }
 
 int charToInt(char c) {
-    switch (c)
-    {
-    case '0':
-        return 0;
-        break;
-    case '1':
-        return 1;
-        break;
-    case '2':
-        return 2;
-        break;
-    case '3':
-        return 3;
-        break;
-    case '4':
-        return 4;
-        break;
-    case '5':
-        return 5;
-        break;
-    case '6':
-        return 6;
-        break;
-    case '7':
-        return 7;
-        break;
-    case '8':
-        return 8;
-        break;
-    case '9':
-        return 9;
-        break;
-    default:
-        return -1;
-        break;
-    }
+    return (c > '0' && c <= '9') ? c - '0' : -1;
 }
 
 int main(int argc, char** argv) {
